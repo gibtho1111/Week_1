c49e4244472acb19628d78db658625e1d726d816
gibtho111 <89614783+gibtho111@users.noreply.github.com>
2022-09-12 16:06:13 +1000

    week 4 vue.js changes

---
+++ tasks/vue.js
@@ -0,0 +1,41 @@
+
+
+
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+
+
+var if_else = new Vue({
+  el: '#if_else',
+  data: {
+    a: false,
+  }
+});
+
+var show = new Vue({
+  el: '#show',
+  data: {
+    a: true,
+  }
+});
+
+
+
+var app6 = new Vue({
+  el:'.card-body',
+  data:{
+    items:[
+      { games: 'CSGO' },
+      { games: 'Total War' },
+      { games: 'Apex: Legends' },
+      { games: 'Minecraft' },
+      { games: 'COD' },
+      { games: 'Minesweeper' },
+    ],
+  }
+})
